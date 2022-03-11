584047dbc151f0d91b677938f4e9ebf48d471c8f
catalindoja <cdojamihai@gmail.com>
2022-03-11 16:50:08 +0100

    Zoo model and migration working properly. Creation of Zoo object in the admin page works also.

--- zooproject/models.py
+++ zooproject/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.urls import reverse
 
-# Create your models here.
+
+class Zoo(models.Model):
+    name = models.CharField(max_length=60)
+    description = models.TextField(blank=True, null=True)
+    max_visitors = models.DecimalField('Max amount of visitors', max_digits=8, decimal_places=0, blank=False, null=False)
+    address = models.CharField(max_length=80)
+    postalcode = models.DecimalField('Postal code', max_digits=8, decimal_places=0, blank=False, null=False)
+
+    def __unicode__(self):
+        return u"%s" % self.name
