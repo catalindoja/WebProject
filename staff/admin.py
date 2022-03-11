2870293c340d5e1713d75e917b65df6ff9685e8d
JoelAmpurda <ampurda98@gmail.com>
2022-03-11 21:19:12 +0100

    staff

--- staff/admin.py
+++ staff/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from staff.models import Staff
 # Register your models here.
+
+admin.site.register(Staff)
--- staff/models.py
+++ staff/models.py
@@ -1,3 +1,12 @@
+from email.policy import default
+from django import forms
 from django.db import models
-
+from django.db.models import Model
+from django.core.validators import MinValueValidator
 # Create your models here.
+
+class Staff(Model):
+    name = models.CharField(max_length=50)
+    # name varchar(50)
+    age = models.IntegerField(default=17, validators=[MinValueValidator(17)])
+    # Create your models here.
