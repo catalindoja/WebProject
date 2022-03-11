1db3e4d6bd3f63135745636d2e64c1445ebf1724
David Castro Sanchez <discktroyer@hotmail.es>
2022-03-11 07:50:09 -0800

    Model visitor created

--- zooproject/models.py
+++ zooproject/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from datetime import date
 
 # Create your models here.
+class Visitor(models.Model):
+    Name = models.CharField(max_length=40,blank=False,null=False)
+    Telephone = models.DecimalField(max_digits=9, decimal_places=0, blank=False,null=False)
+    Email = models.EmailField(blank=False,null=False)
+    Age = models.DecimalField(max_digits=3, decimal_places=0, blank=False,null=False)
+    DateLatestVisit = models.DateField(default=date.today,blank=False,null=False)
+    LatestZooVisited = models.IntegerField(blank=False,null=False)
