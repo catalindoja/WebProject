d09f4621fe1fa9f65af8fa77afab597e61b27919
catalindoja <cdojamihai@gmail.com>
2022-03-15 17:15:44 +0100

    Changes to model. All primary and foreign keys created successfully

--- zooproject/models.py
+++ zooproject/models.py
@@ -1,7 +1,4 @@
 from django.db import models
-from django.contrib.auth.models import User
-from django.urls import reverse
-from django.core.validators import MinValueValidator
 from datetime import date
 
 
@@ -17,23 +14,13 @@ class Zoo(models.Model):
         return u"%s" % self.name
 
 
-class Animal(models.Model):
-    animal_id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=80)
-    #zoo_id = get id from Zoo class
-    #caretaker_id = get id from Staff class
-    #veterinary_id = get id from Veterinary class
-
-    def __unicode__(self):
-        return u"%s" % self.name
-
-
 class Worker(models.Model):
-    id = models.AutoField(primary_key=True)
+    worker_id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=80)
     age = models.DateField(default=date.today, blank=False, null=False)
     address = models.CharField(max_length=80)
     postalcode = models.DecimalField('Postal code', max_digits=8, decimal_places=0, blank=False, null=False)
+    zoo_id = models.ForeignKey(Zoo, null=True, on_delete=models.CASCADE)
     # ??? phone_number = PhoneNumberField(null=False, blank=False, unique=True) # from phonenumber_field.modelfields import PhoneNumberField # pip install django-phonenumber-field
     # ??? zoo_id = get id from Zoo class
 
@@ -42,7 +29,6 @@ class Worker(models.Model):
 
 
 class Staff(Worker):
-    staff_id = models.AutoField(primary_key=True)
     assigned_habitat = models.CharField(max_length=80, default="Dolphins")
 
     def __unicode__(self):
@@ -50,22 +36,32 @@ class Staff(Worker):
 
 
 class Veterinary(Worker):
-    vet_id = models.AutoField(primary_key=True)
-    assigned_animals = models.IntegerField(null=False, blank=False)
+    number_assigned_animals = models.IntegerField(null=False, blank=False)
     # name varchar(50)
 
+    def __unicode__(self):
+        return u"%s" % self.name
+
+
+class Animal(models.Model):
+    animal_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=80)
+    zoo_id = models.ForeignKey(Zoo, null=True, on_delete=models.CASCADE)
+    staff_id = models.ForeignKey(Staff, null=True, on_delete=models.CASCADE)
+    veterinary_id = models.ForeignKey(Veterinary, null=True, on_delete=models.CASCADE)
 
     def __unicode__(self):
         return u"%s" % self.name
 
 
 class Visitor(models.Model):
+    visitor_id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=40,blank=False,null=False)
     telephone = models.DecimalField(max_digits=9, decimal_places=0, blank=False,null=False)
     email = models.EmailField(blank=False,null=False)
     age = models.DecimalField(max_digits=3, decimal_places=0, blank=False,null=False)
     dateLatestVisit = models.DateField(default=date.today,blank=False,null=False)
-    latestZooVisited = models.IntegerField(blank=False,null=False)
+    zoo_id = models.ForeignKey(Zoo, null=True, on_delete=models.CASCADE)
 
     def __unicode__(self):
         return u"%s" % self.name
