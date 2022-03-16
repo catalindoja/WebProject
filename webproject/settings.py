5f6f9f3bf26d55b489d460c428d3caad3deda926
JoelAmpurda <ampurda98@gmail.com>
2022-03-16 17:54:59 +0100

    heroku

--- webproject/settings.py
+++ webproject/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_#o6w&y$ipxj-isokw3%fg$ju=3$r!vlqjqqc^95qlf67tw$rv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['limitless-reaches-64342.herokuapp.com']
 
 
 # Application definition
