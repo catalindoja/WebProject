6f34b5ca46a7b7d534714235a5f660ee6611b439
David-Castro-Sanchez <discktroyer@hotmail-es>
2022-03-19 08:38:54 -0700

    Html and urls for login

--- webproject/settings.py
+++ webproject/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-#import django_heroku
+import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -124,4 +124,4 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-#django_heroku.settings(locals())
+django_heroku.settings(locals())
--- webproject/urls.py
+++ webproject/urls.py
@@ -1,3 +1,4 @@
+from zooproject import views
 """webproject URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
@@ -13,12 +14,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
+from django.contrib import admin,auth
+from django.contrib.auth import views as auth_views
 from django.urls import path, include
-import zooproject.views
+from zooproject import views
 
 urlpatterns = [
-    path('zooproject/', include('zooproject.urls')),
+    path('', auth_views.LoginView.as_view(template_name='registration/login.html',next_page='/admin')),
     path('admin/', admin.site.urls),
+
 
 ]
