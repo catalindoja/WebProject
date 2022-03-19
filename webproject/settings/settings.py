fe48230856de175def18635120ae1afa774ed00a
JoelAmpurda <ampurda98@gmail.com>
2022-03-19 16:08:48 +0100

    heroku settings

--- webproject/settings/settings.py
+++ webproject/settings/settings.py
@@ -11,7 +11,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 #import django_heroku
 from pathlib import Path
-
+ 
+import os 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
@@ -102,7 +103,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+#STATIC_URL = 'static/'
+
+STATIC_ROOT = BASE_DIR
+STATIC_URL = '/static/'
+STATICFILES_DIRS = ( os.path.join('static'), )
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
