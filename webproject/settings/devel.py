e6dbacb66d69e18c9ce64e97167ab97d877be976
JoelAmpurda <ampurda98@gmail.com>
2022-03-21 16:06:03 +0100

    docker changes

--- webproject/settings/devel.py
+++ webproject/settings/devel.py
@@ -5,6 +5,13 @@ from . settings import *
 
 DATABASES = {
     'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': "django",
+        'USER': "django",
+        'PASSWORD': "secret",
+        'HOST': "django-postgre",
+        'PORT': '5432',
+    }, 'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
@@ -13,4 +20,4 @@ DATABASES = {
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '0.0.0.0']
