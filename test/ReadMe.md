a96787047946c60d33f4d631a990073353a8d48c
aiax <aiax.developer@gmail.com>
2022-05-02 18:07:08 +0200

    Add testing environment

---
+++ test/ReadMe.md
@@ -0,0 +1,3 @@
+To do before executing tests:
+    1. Install Chrome if you don't have it already
+    2. Install ChromeDriver: https://www.codegrepper.com/code-examples/shell/how+to+install+chromedriver+in+ubuntu 
--- webproject/settings/settings.py
+++ webproject/settings/settings.py
@@ -32,7 +32,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'zooproject'
+    'zooproject',
+    'behave_django',
+
 ]
 
 MIDDLEWARE = [
