2a81efe8fe033177a2b28fa0f66cc0e9b9a5b14f
catalindoja <cdojamihai@gmail.com>
2022-05-07 17:42:42 +0200

    Modifying the navbar and implemented an iffy logout and a homepage

--- zooproject/urls.py
+++ zooproject/urls.py
@@ -7,9 +7,11 @@ from django.urls import path, include
 
 
 urlpatterns = [
-    path('', auth_views.LoginView.as_view(template_name='registration/login.html',next_page='/list_data')),
+    path('', auth_views.LoginView.as_view(template_name='registration/login.html',next_page='/home')),
     path('registration/register/', views.signup),
     path('list_data', views.list_data),
+    path('home', views.home, name='home'),
+    path('logout', views.logout, name='logout'),
     path('registration/register_veterinary', SignupVeterinaryView.as_view(), name='registration/register_veterinary'),
     path('registration/register_staff', SignupStaffView.as_view(), name='registration/register_staff'),
     path('registration/register_visitor', SignupVisitorView.as_view(), name='registration/register_visitor'),
