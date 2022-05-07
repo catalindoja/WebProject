7705e666d1fab0b90c1f2700c2ecf721f8b85218
pelayocobos <pelayo.cobos@gmail.com>
2022-05-07 17:56:35 +0200

    Fixes navbar links and zoo template

--- zooproject/urls.py
+++ zooproject/urls.py
@@ -17,8 +17,8 @@ urlpatterns = [
     path('registration/register_visitor', SignupVisitorView.as_view(), name='registration/register_visitor'),
     path('create_animal', CreateAnimalView.as_view(), name='create_animal'),
     path('animal_editor/<str:pk>/', views.updateAnimal, name='edit/edit_animal'),
-    path('animal_editor/', views.list_animals, name='edit/list_animals'),
-    path('animal_delete/', views.list_animals_delete, name='edit/list_animals2'),
+    path('animal_editor/', views.list_animals, name='animal_editor'),
+    path('animal_delete/', views.list_animals_delete, name='animal_delete'),
     path('animal_delete/<str:pk>/', views.deleteAnimal, name='delete/delete_animal'),
     path('create_zoo', CreateZooView.as_view(), name='create_zoo'),
 
