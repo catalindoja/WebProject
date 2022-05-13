f4986339e28e4e78e3723854e3f2adab2f462f2f
aiax <aiax.developer@gmail.com>
2022-05-13 16:23:51 +0200

    Commit current tests to merge with new stuff

--- features/register_veterinary.feature
+++ features/register_veterinary.feature
@@ -1,17 +1,34 @@
 Feature: Register Veterinary
 In order to keep track of the zoo veterinaries,
-As an admin or veterinary?,
+As a veterinary,
 I want to register a veterinary in the corresponding zoo with its personal details.
 
  Background: There is a registered user
     Given Exists a user "admin" with password "admin"
+    Given I am on the Django Admin
 
   Scenario: Register just veterinary name
     Given I login as user "admin" with password "admin"
     When I register veterinary
-      | name        |
+      | username        |
       | Joan        |
-    Then I'm viewing the details page for restaurant by "user"
+      | id_password1        |
+      | 1234        |
+      | id_password2        |
+      | 1234        |
+      | id_name        |
+      | Joan        |
+      | id_age        |
+      | 10        |
+      | id_address        |
+      | Hola        |
+      | id_postalcode        |
+      | 25600        |
+      | id_zoo_id        |
+      | 1        |
+      | id_number_assigned_animals        |
+      | 10        |
+    Then I'm viewing the admin page
       | name        |
       | Joan        |
     And There are 1 veterinaries
--- features/steps/register_veterinary.py
+++ features/steps/register_veterinary.py
@@ -1,27 +1,70 @@
 from behave import *
-import operator
 from functools import reduce
 from django.db.models import Q
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import Select
+import time
 
-use_step_matcher("parse")
 
-@when(u'I register restaurant')
+use_step_matcher("re")
+
+@given(u'I am on the Django Admin')
 def step_impl(context):
-    for row in context.table:
-        context.browser.visit(context.get_url('myrestaurants:restaurant_create'))
-        form = context.browser.find_by_tag('form').first
-        for heading in row.headings:
-            context.browser.fill(heading, row[heading])
-        form.find_by_value('Submit').first.click()
+    chrome_options = Options()
+    #chrome_options.add_argument("--headless")
+    context.selenium = webdriver.Chrome(options=chrome_options)
 
-@then(u'I\'m viewing the details page for restaurant by "{user}"')
-def step_impl(context, user):
-    q_list = [Q((attribute, context.table.rows[0][attribute])) for attribute in context.table.headings]
-    from zooproject.models import Restaurant
-    restaurant = Restaurant.objects.filter(reduce(operator.and_, q_list)).get()
-    assert context.browser.url == context.get_url(restaurant)
+    # Login to the Admin Panel
+    context.selenium.get(f'http://127.0.0.1:8000/admin/')
 
-@then(u'There are {count:n} restaurants')
-def step_impl(context, count):
-    from zooproject.models import Restaurant
-    assert count == Restaurant.objects.count()
+    # Fill Login Information
+    username = context.selenium.find_element_by_id("id_username")
+    username.send_keys("admin")
+    password = context.selenium.find_element_by_id("id_password")
+    password.send_keys("admin")
+
+    # Locate login button and click on it
+    context.selenium.find_element_by_xpath('//input[@value="Log in"]').click()
+    context.test.assertEquals(context.selenium.title, "Site administration | Django site admin")
+
+@when(u'I register veterinary')
+def step_impl(context):
+    chrome_options = Options()
+    #chrome_options.add_argument("--headless")
+    context.selenium = webdriver.Chrome(options=chrome_options)
+
+    # Login to the Admin Panel
+    context.selenium.get(f'http://127.0.0.1:8000/registration/register_veterinary')
+    username = context.selenium.find_element_by_id("id_username")
+    username.send_keys("Test_guy")
+    password1 = context.selenium.find_element_by_id("id_password1")
+    password1.send_keys("prova1234@")
+    password2 = context.selenium.find_element_by_id("id_password2")
+    password2.send_keys("prova1234@")
+
+    name = context.selenium.find_element_by_id("id_name")
+    name.send_keys("Test_guy")
+    age = context.selenium.find_element_by_id("id_age")
+    age.send_keys(25)
+    address = context.selenium.find_element_by_id("id_address")
+    address.send_keys("Carrer prova")
+    postalCode = context.selenium.find_element_by_id("id_postalcode")
+    postalCode.send_keys("25000")
+    
+    zooId = Select(context.selenium.find_element_by_id('id_zoo_id'))
+    zooId.select_by_visible_text("Zoo 1")
+    
+    nAssignedAnimals = context.selenium.find_element_by_id("id_number_assigned_animals")
+    nAssignedAnimals.send_keys("20")
+
+    context.selenium.find_element_by_xpath('/html/body/div/form/input[2]').click()
+
+
+@then(u'I\'m viewing the admin page')
+def step_impl(context):
+    print("hola")
+
+@then(u'There are 1 veterinaries')
+def step_impl(context):
+    print("hola")
